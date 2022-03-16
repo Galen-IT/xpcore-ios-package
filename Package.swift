49e848ead4acc78910cd795960787a483310a0d3
alexis <alexis@wildapricot.com>
2022-03-16 17:50:58 +0300

    exporting module

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "GalenitXpCore",
-            targets: ["GalenitXpCore111"]),
+            targets: ["GalenitXpCore111", "CoreUtils", "galenit_device_api"]),
     ],
     targets: [
         .target(
@@ -23,11 +23,11 @@ let package = Package(
             name: "CoreUtils",
             url: "https://github.com/Galen-IT/FlowVit_PublicSDKs_iOS/releases/download/v1.0.0/CoreUtils.xcframework.zip",
             checksum: "9573e561e8f07761290e11d57ca58269a1ea5e74355bb2feb6e1ef1cad45ab77"
+        ),
+       .binaryTarget(
+            name: "galenit_device_api",
+            url: "https://github.com/Galen-IT/FlowVit_PublicSDKs_iOS/releases/download/v1.0.0/galenit_device_api.xcframework.zip",
+            checksum: "875104293d48b43a5115a0c0789da341331e9bcb71b4de9164e49b2aae9842fa"
         )
-//       .binaryTarget(
-//            name: "galenit_device_api",
-//            url: "https://github.com/Galen-IT/FlowVit_PublicSDKs_iOS/releases/download/v1.0.0/galenit_device_api.xcframework.zip",
-//            checksum: "875104293d48b43a5115a0c0789da341331e9bcb71b4de9164e49b2aae9842fa"
-//        )
     ]
 )
