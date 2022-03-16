b464ca6ff84abd4886147b0cdd75c75add9a2d54
alexis <alexis@wildapricot.com>
2022-03-16 16:23:09 +0300

    xpCore setup

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GalenitXpCore",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "GalenitXpCore",
+            targets: ["galenit_device_api"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "galenit_device_api",
+            url: "https://github.com/Galen-IT/FlowVit_PublicSDKs_iOS/releases/download/v1.0.0/galenit_device_api.xcframework.zip",
+            checksum: "875104293d48b43a5115a0c0789da341331e9bcb71b4de9164e49b2aae9842fa"
+        )
+    ]
+)
