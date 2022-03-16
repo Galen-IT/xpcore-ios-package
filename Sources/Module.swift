8ca0764915f7c97bf3072cc8bb9dbb77967ebba7
alexis <alexis@wildapricot.com>
2022-03-16 17:00:30 +0300

    exporting module

--- Sources/Module.swift
+++ Sources/Module.swift
@@ -1,7 +1,7 @@
 import galenit_device_api
 
 public struct Helper {
-    public var hello: String { "hello" }
+    public var hello: String { "hello2" }
     
     public func initCore(key: String) {
         let res = GalenIT.shared.start(token: key)
