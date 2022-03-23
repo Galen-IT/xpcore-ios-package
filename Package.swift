3d8301f1b3a1e58b6421e654e02327c393c21a80
Ivan O <ioparin@pm.me>
2022-03-24 00:54:48 +0400

    updating product name

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "GalenitXpCore",
+            name: "galenit_device_api",
             targets: ["GalenitXpCore"]
         ),
     ],
