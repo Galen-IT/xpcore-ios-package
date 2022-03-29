22b9b36f18e90bf08daf5904947a6e21eeae8732
Ivan O <ioparin@pm.me>
2022-03-29 21:18:48 +0400

    package manifest finalization

--- Package.swift
+++ Package.swift
@@ -1,16 +1,15 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "GalenitXpCore",
+    name: "galenit-xpcore",
     platforms: [
         .iOS(.v14)
     ],
     products: [
         .library(
-            name: "galenit_device_api",
+            name: "GalenitXpCore",
             targets: ["GalenitXpCore"]
         ),
     ],
@@ -22,7 +21,7 @@ let package = Package(
         ),
        .binaryTarget(
             name: "galenit_device_api",
-            url: "https://github.com/Galen-IT/FlowVit_PublicSDKs_iOS/releases/download/v1.0.0/galenit_device_api.xcframework.zip",
+            url: "https://github.com/galen-it/xpcore-ios-package/releases/download/v1.0.0/galenit_device_api.xcframework.zip",
             checksum: "0f29be7ead4a387b76f07f37cea585386e03b4c9a9e53291f18afef752882fe1"
         )
     ]
