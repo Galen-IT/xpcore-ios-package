72fc7a520ac2176551ec9493cdaefe05169ad56e
iv-ann <98310998+iv-ann@users.noreply.github.com>
2022-08-23 13:39:49 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         ),
        .binaryTarget(
             name: "galenit_device_api",
-            url: "https://github.com/galen-it/xpcore-ios-package/releases/download/1.1.1/galenit_device_api.xcframework.zip",
+            url: "https://github.com/galen-it/xpcore-ios-package/releases/download/1.1.2/galenit_device_api.xcframework.zip",
             checksum: "500bd0f0fe1c79c8ccfd6a1ec9c67e88fc404ae3e928c992b98762da9d82b7a2"
         )
     ]
